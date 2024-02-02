821a8a6a781c04ebb60250443278902387a6bf8f
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2024-02-02 15:12:44 +0100

    play audio\

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const gridContainer = document.querySelector('.grid-container');
+const audioInput = document.getElementById('audioInput');
+const audioPlayer = document.getElementById('audioPlayer');
 
-        for (let i = 0; i < 100; i++) { 
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid-item');
-            gridContainer.appendChild(gridItem);
+    for (let i = 0; i < 100; i++) { 
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
+    }
+
+    audioInput.addEventListener('change', function() {
+        const selectedFile = audioInput.files[0];
+
+        if (selectedFile) {
+            const objectURL = URL.createObjectURL(selectedFile);
+            audioPlayer.src = objectURL;
         }
+    });
